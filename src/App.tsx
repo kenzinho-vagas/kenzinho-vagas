6a9f6f9fea14a35115ed5094c42899f1a2edba04
Vinicius Pacheco <viniciusgrp@gmail.com>
2022-11-01 13:24:36 -0300

    Fix error on headers link

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import Global from './styles/Global';
 import './App.css';
 import Header from './components/Header';
 import DashboardAdmin from './pages/DashboardAdmin';
+import RoutesMain from './routes';
 
 function App() {
   return (
     <div className="App">
       <Global />
-      <DashboardAdmin/>
+      <RoutesMain/>
     </div>
   );
 }
--- src/styles/Links/index.ts
+++ src/styles/Links/index.ts
@@ -4,33 +4,45 @@ import styled from "styled-components";
 export const LinkPrimary = styled(Link)`
     background-color: var(--color-primary);
     padding: 5px 10px;
-    width: 80px;
+    width: 60px;
     height: 30px;
     color: white;
     font-weight: 700;
     text-transform: uppercase;
     border-radius: 5px;
-    border-color: var(--color-primary);
+    border: 1px solid  var(--color-primary);
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 12px;
 
     @media screen and (min-width: 768px){
         width: 130px;
         height: 40px;
+        font-size: 18px;
     }
 `
 
 export const LinkSecundary = styled(Link)`
     background-color: white;
     padding: 5px 10px;
-    width: 80px;
+    width: 60px;
     height: 30px;
     color: var(--color-primary);
     font-weight: 700;
     text-transform: uppercase;
     border-radius: 5px;
-    border-color: var(--color-primary);
+    border: 1px solid  var(--color-primary);;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 12px;
 
     @media screen and (min-width: 768px){
         width: 130px;
         height: 40px;
+        font-size: 18px;
     }
 `
