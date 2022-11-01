360d9020f5064076596df20163fc87d5f5cb2791
Natalia <natpereira94@gmail.com>
2022-11-01 20:36:08 -0300

    feat: register

---
+++ src/pages/Register/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const DivRegister = styled.div`
+width: 100%;
+height: 100%;
+display: flex;
+align-items: center;
+justify-content: center;
+margin-top: 5%;
+`
