597386d9b6ce50eeffeda688f171312c9f9320f6
Luiza Barros <thecoderana@gmail.com>
2022-11-02 11:58:11 -0300

    feat: add filter by level

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -22,12 +22,15 @@ interface ICardsProps {
 const Cards = ({ title }: ICardsProps) => {
     const [allJobs, setAllJobs] = useState<IJobsProps[] | []>([])
     const [savedJobs, setSavedJobs] = useState<IJobsProps[] | []>([])
+    const [user, setUser] = useState<{level: string}>({level: "Júnior"})
 
     useEffect(() => {
         async function getAllJobs() {
             try {
                 const { data } = await api.get<IJobsProps[]>("/companyJobs")
-                setAllJobs(data)
+
+                const filteredData = data.filter(object => object.level === user.level)
+                setAllJobs(filteredData)
             } catch (error) {
                 console.error(error)
             }
@@ -37,11 +40,25 @@ const Cards = ({ title }: ICardsProps) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
-    if (title === "Todas as vagas") {
-        return <Lists objectArray={allJobs}/>
-    } else {
-        return <Lists objectArray={savedJobs}/>
-    }
+    useEffect(() => {
+        async function getSavedJobs() {
+            try {
+                const userID = 4 //localStorage.getItem("")
+                const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imx1YW5AbWFpbC5jb20iLCJpYXQiOjE2NjczOTc4MTgsImV4cCI6MTY2NzQwMTQxOCwic3ViIjoiNCJ9.xdoNbqjZpme6K7QqtiSPeJyB-uXReUKACd8tyLYmkn0" // localStorage.getItem("")
+                api.defaults.headers.authorization = `Bearer ${token}`
+
+                const { data } = await api.get<IJobsProps[]>(`/users/${userID}/jobs`)
+                setSavedJobs(data)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        getSavedJobs()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    return title === "Todas as vagas" ? <Lists objectArray={allJobs}/> : <Lists objectArray={savedJobs}/>
 }
 
 export default Cards
