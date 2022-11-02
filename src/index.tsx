e547895e8a93240de19af4d5fa3b79c5630d73b7
Luiza Barros <thecoderana@gmail.com>
2022-11-02 10:29:21 -0300

    fix: contexts not found

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import Contexts from "./contexts";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Contexts>
         <App />
-      </Contexts>
     </BrowserRouter>
   </React.StrictMode>
 );
