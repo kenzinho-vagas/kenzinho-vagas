093e2c3c93424bbd1f72d4e0752680698aa01ec6
Daniel Heringer <danielheringers@icloud.com>
2022-11-03 13:04:18 -0300

    fix newJob

--- src/components/CreateJob/index.tsx
+++ src/components/CreateJob/index.tsx
@@ -14,6 +14,7 @@ export interface INewJobForm {
   level: string;
   jobURL: string;
   description: string;
+  candidates: [];
 }
 
 const CreateJob = () => {
--- src/contexts/JobContext/index.tsx
+++ src/contexts/JobContext/index.tsx
@@ -27,22 +27,25 @@ export interface IFormVagas {
   level: string;
   jobURL: string;
   description: string;
+  candidates: [];
 }
 
 export const JobContext = createContext<IJobContext>({} as IJobContext);
 
 export const JobProvider = ({ children }: IJobProvider) => {
   const [jobs, setJob] = useState<IFormVagas | null>(null);
+  const [techs, setTechs] = useState<ITechs | null>(null);
   const [adminJobs, setAdminJobs] = useState<IFormVagas | null>();
 
   async function NewJob(data: INewJobForm) {
     try {
       const response = await api.post("users/1/companyJobs ", data);
       console.log(response);
-      const { jobs: jobResponse, token } = response.data;
+      const { token } = response.data;
       api.defaults.headers.authorization = `Bearer ${token}`;
-      setJob(jobResponse);
-      console.log(jobResponse);
+      const techsJob = response.data.tech.split(",");
+      setJob(response.data);
+      setTechs(techsJob);
     } catch (error) {
       console.log(error);
     }
