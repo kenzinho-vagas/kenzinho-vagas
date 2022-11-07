42f766d492a8485b98f9aef4b6228b2516704086
netomilfont <netomilfont@hotmail.com>
2022-11-07 14:49:49 -0300

    feat: fix css Header

--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -6,23 +6,25 @@ export const DivLogin = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-top: 5%;
-  
-  .contentLogin{
-    width:489px;
-    border-radius:8px;
+  margin-top: 70px;
+
+  .contentLogin {
+    width: 489px;
+    border-radius: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 
-  .title{
-    text-align:center;
+  .title {
+    text-align: center;
     margin: 20px;
     font-weight: bolder;
     font-size: 30px;
   }
 
-  .account{
+  .account {
     text-align: center;
     margin: 20px;
-  } 
-
-  `
+  }
+`;
--- src/styles/Links/index.ts
+++ src/styles/Links/index.ts
@@ -1,48 +1,49 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const LinkPrimary = styled(Link)`
-    background-color: var(--color-primary);
-    padding: 5px 10px;
-    width: 60px;
-    height: 30px;
-    color: white;
-    font-weight: 700;
-    text-transform: uppercase;
-    border-radius: 5px;
-    border: 1px solid  var(--color-primary);
-    text-decoration: none;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 12px;
+  background-color: var(--color-primary);
+  padding: 5px 10px;
+  width: 60px;
+  height: 30px;
+  color: white;
+  font-weight: 700;
+  text-transform: uppercase;
+  border-radius: 5px;
+  border: 1px solid var(--color-primary);
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 0.75rem;
+  text-decoration: none;
 
-    @media screen and (min-width: 768px){
-        width: 130px;
-        height: 40px;
-        font-size: 18px;
-    }
-`
+  @media screen and (min-width: 768px) {
+    width: 130px;
+    height: 40px;
+    font-size: 1rem;
+  }
+`;
 
 export const LinkSecundary = styled(Link)`
-    background-color: white;
-    padding: 5px 10px;
-    width: 60px;
-    height: 30px;
-    color: var(--color-primary);
-    font-weight: 700;
-    text-transform: uppercase;
-    border-radius: 5px;
-    border: 1px solid  var(--color-primary);;
-    text-decoration: none;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 12px;
+  background-color: white;
+  padding: 5px 10px;
+  width: 60px;
+  height: 30px;
+  color: var(--color-primary);
+  font-weight: 700;
+  text-transform: uppercase;
+  border-radius: 5px;
+  border: 1px solid var(--color-primary);
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 0.75rem;
 
-    @media screen and (min-width: 768px){
-        width: 130px;
-        height: 40px;
-        font-size: 18px;
-    }
-`
+  @media screen and (min-width: 768px) {
+    width: 130px;
+    height: 40px;
+    font-size: 1rem;
+  }
+`;
