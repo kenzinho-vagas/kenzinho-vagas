79933e941d73da87f19f772129dd6f2551b6d2a8
Vinicius Pacheco <viniciusgrp@gmail.com>
2022-11-07 12:51:49 -0300

    fix duplicated level on candidates cards

--- src/pages/DashboardAdmin/index.tsx
+++ src/pages/DashboardAdmin/index.tsx
@@ -1,42 +1,53 @@
-import Candidates from "../../components/Candidates"
-import CreateJob from "../../components/CreateJob"
-import Header from "../../components/HeaderAdmin"
-import JobsAdmin from "../../components/FilterJobsAdmin"
-import JobsCreateds from "../../components/JobsCreateds"
-import Welcome from "../../components/Welcome"
-import { useContext, useEffect } from "react"
-import { JobContext } from "../../contexts/JobContext"
-import ModalEdit from "../../components/ModalEditJob"
-import api from "../../services/api"
+import Candidates from "../../components/Candidates";
+import CreateJob from "../../components/CreateJob";
+import Header from "../../components/HeaderAdmin";
+import JobsAdmin from "../../components/FilterJobsAdmin";
+import JobsCreateds from "../../components/JobsCreateds";
+import Welcome from "../../components/Welcome";
+import { useContext, useEffect } from "react";
+import { JobContext } from "../../contexts/JobContext";
+import ModalEdit from "../../components/ModalEditJob";
+import api from "../../services/api";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const DashboardAdmin = () => {
     const { jobId, editModal } = useContext(JobContext);
+    const [admin, setAdmin] = useState<boolean>(false)
 
-    async function isAdmin() {
-        const id = localStorage.getItem('@kenzinhoVagas:id')
-        try {
-            const {data} = await api.get(`users/${id}`)
-            console.log(data)
-        } catch (error) {
-            
-        }
+  const navigate = useNavigate();
+
+  async function isAdmin() {
+    const id = localStorage.getItem("@kenzinhoVagas:id");
+    try {
+      const { data } = await api.get(`users/${id}`);
+      if (!data.isAdmin) {
+        console.log("nao é admin");
+        return navigate("/dashboardUser");
+      } else {
+          setAdmin(true)
+      }
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    useEffect(() => {
-        isAdmin()
-    },[])
+  useEffect(() => {
+    isAdmin();
+  }, []);
 
     return (
-        <>
+        admin ? (
+            <>
             <Header />
-            <Welcome name="Admin"/>
+            <Welcome name="Admin" />
             <CreateJob />
             <JobsAdmin />
             <JobsCreateds />
             {jobId && <Candidates />}
-            {editModal && <ModalEdit/>}
-        </>
+            {editModal && <ModalEdit />}
+          </>
+      ) : <h1>Loading...</h1>
     )
-}
-
+};
 export default DashboardAdmin;
