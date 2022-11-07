a592204f8324de1b554bd9bbae698c539755598f
netomilfont <netomilfont@hotmail.com>
2022-11-07 14:52:03 -0300

    feat: add footer to login page

--- src/components/Footer/style.ts
+++ src/components/Footer/style.ts
@@ -1,15 +1,17 @@
 import styled from "styled-components";
 
-export const DivFooter = styled.div `
-    background-color: var(--color-primary);
-    display: flex;
-    justify-content: flex-end;
+export const DivFooter = styled.div`
+  background-color: var(--color-primary);
+  display: flex;
+  justify-content: flex-end;
+  padding: 12px;
+  bottom: 0;
+  position: fixed;
+  width: 100vw;
 
-    padding: 12px;
-
-    p {
-        font-size: 12px;
-        font-weight: 700;
-        color: #ffffff;
-    }
-`
+  p {
+    font-size: 12px;
+    font-weight: 700;
+    color: #ffffff;
+  }
+`;
