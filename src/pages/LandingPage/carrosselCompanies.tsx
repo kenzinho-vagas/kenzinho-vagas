872516b83df06b659610ea5a6b6842cb52d1f98f
Luiza Barros <thecoderana@gmail.com>
2022-11-08 12:15:09 -0300

    fix: duplicate values

--- src/pages/LandingPage/carrosselCompanies.tsx
+++ src/pages/LandingPage/carrosselCompanies.tsx
@@ -8,27 +8,28 @@ interface ICompany {
     password: string,
     name: string,
     isAdmin: boolean,
-    id: number
+    id: number;
 }
 
 interface ICompanyInfo {
   name: string, 
-  jobs: number
+  jobs: number;
 }
 
 export const ControlledCarousel = () => {
   const [index, setIndex] = useState(0);
-  const [allCompanies, setAllCompanies] = useState<ICompany[] | []>([])
-  const [companiesData, setCompaniesData] = useState<ICompanyInfo[]| []>([])
+  const [allCompanies, setAllCompanies] = useState<ICompany[] | []>([]);
+  const [companiesData, setCompaniesData] = useState<ICompanyInfo[]| []>([]);
+  const [uniqueCompaniesData, setUniqueCompaniesData] = useState<ICompanyInfo[]| []>([]);
 
   useEffect(() => {
     async function getCompanies() {
         if (allCompanies) {
           try {
-            const { data } = await api.get<ICompany[]>("/users?isAdmin=true")
-            setAllCompanies(data)
+            const { data } = await api.get<ICompany[]>("/users?isAdmin=true");
+            setAllCompanies(data);
           } catch (error) {
-            console.error(error)
+            console.error(error);
           }
         }
     }
@@ -40,16 +41,26 @@ export const ControlledCarousel = () => {
     async function getJobsCompanies() {
       try {
         allCompanies.forEach(async (company) => {
-          const { data } = await api.get<IJobsProps[]>(`/companyJobs?company_name=${company.name}`)
-          setCompaniesData((previousData) => [...previousData, {name: company.name, jobs: data.length}])
+          const { data } = await api.get<IJobsProps[]>(`/companyJobs?company_name=${company.name}`);
+          setCompaniesData((previousData) => [...previousData, {name: company.name, jobs: data.length}]);
         })
       } catch (error) {
-          console.error(error)
+          console.error(error);
       }
     }
     
     getJobsCompanies()
   }, [allCompanies])
+
+  useEffect(() => {
+    const dataNotDuplicated = Array.from(new Set(companiesData.map(company => company.name)))
+    .map(nameEl => {
+      return companiesData.find(company => company.name === nameEl)
+    })
+
+    console.log(dataNotDuplicated)
+    setUniqueCompaniesData(dataNotDuplicated)
+  }, [])
   
   const handleSelect = (selectedIndex: SetStateAction<number>) => {
     setIndex(selectedIndex);
@@ -58,7 +69,7 @@ export const ControlledCarousel = () => {
   return (
     <Carousel activeIndex={index} onSelect={handleSelect} slide={false} variant="dark">
         {
-            companiesData.map((company, index) => {
+            uniqueCompaniesData.map((company, index) => {
                 return (
                   <Carousel.Item key={ index }>
                       <h2>{ company.name }</h2>
