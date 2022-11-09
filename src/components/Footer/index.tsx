50ae8684308401568dbd18007a8f9d14f617159f
Daniel Heringer <danielheringers@icloud.com>
2022-11-09 14:16:25 -0300

    style organization

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,11 @@
-import { DivFooter } from "./style";
+import { DivFooter } from "../../styles/Footer/style";
 
 const Footer = () => {
-
-    return (
-       <DivFooter>
-        <p>© 2022 - 2022 KenzinhoVagas. - Todos os direitos reservados.</p>
-       </DivFooter>
-    )
-}
+  return (
+    <DivFooter>
+      <p>© 2022 - 2022 KenzinhoVagas. - Todos os direitos reservados.</p>
+    </DivFooter>
+  );
+};
 
 export default Footer;
