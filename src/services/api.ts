43ed3b7cb0a030da3e943e38bb6f663855b23a35
Luiza Barros <88062746+luizabarros@users.noreply.github.com>
2023-01-20 18:39:19 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://kenzinho-vagas.herokuapp.com/",
+  baseURL: "https://gifted-toad-veil.cyclic.app",
 });
 
 export default api;
